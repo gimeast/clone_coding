3891868180f4d60d1656f8d5f2f0722b3d9f5978
gimeast <slasnrndu@gmail.com>
2025-12-02 23:43:05 +0900

    brunch 메인페이지 클론 코딩
    - Intro 컴포넌트 작업

--- brunch/src/pages/home/ui/Home.tsx
+++ brunch/src/pages/home/ui/Home.tsx
@@ -1,10 +1,14 @@
 import { Footer, Header, SideMenu, TopBanner } from '@/widgets';
+import Intro from '@/pages/home/ui/Intro.tsx';
 
 const Home = () => {
     return (
         <>
             <TopBanner />
             <Header />
+            <article>
+                <Intro />
+            </article>
             <SideMenu />
             <Footer />
         </>
