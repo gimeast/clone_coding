1a106ae0d4c7e0c7f458cc0dea46e1ef0b2c061e
gimeast <slasnrndu@gmail.com>
2026-01-29 00:50:42 +0900

    SVG 파일 컴포넌트화 및 레이아웃 검색 바 추가 작업
    - SVG 파일(`logo.svg`, `search.svg`) 추가 및 컴포넌트화
    - 로그인 전 및 후 레이아웃에 `Logo` 컴포넌트 적용
    - 로그인 후 메인 레이아웃에 검색 바 UI 및 스타일 추가
    - `@svgr/webpack` 설정 추가 (turbopack 대응)

--- x-com/src/app/(afterLogin)/layout.tsx
+++ x-com/src/app/(afterLogin)/layout.tsx
@@ -1,14 +1,33 @@
 import style from './layout.module.css';
+import Logo from '@/assets/logo.svg';
+import Search from '@/assets/search.svg';
 
 import { ReactNode } from 'react';
+import Link from 'next/link';
 
 const Layout = ({ children }: { children: ReactNode }) => {
     return (
         <div className={style.container}>
-            <header className={style.leftHeader}></header>
+            <header className={style.leftHeader}>
+                <h1 className={style.logo}>
+                    <Link href='/home'>
+                        <Logo />
+                    </Link>
+                </h1>
+            </header>
             <main className={style.main}>
                 <section className={style.mainSection}>{children}</section>
-                <section className={style.rightSection}></section>
+                <section className={style.rightSection}>
+                    <div className={style.searchWrapper}>
+                        <div className={style.searchBorder}>
+                            <Search />
+                            <label className='sr-only' htmlFor='search'>
+                                검색
+                            </label>
+                            <input type='text' id='search' />
+                        </div>
+                    </div>
+                </section>
             </main>
         </div>
     );
--- x-com/src/app/(beforeLogin)/_component/Main.tsx
+++ x-com/src/app/(beforeLogin)/_component/Main.tsx
@@ -1,3 +1,4 @@
+import Logo from '@/assets/logo.svg';
 import style from './main.module.css';
 import Link from 'next/link';
 
@@ -5,18 +6,7 @@ const Main = () => {
     return (
         <main className={style.container}>
             <h1 className={style.left}>
-                <svg
-                    viewBox='0 0 24 24'
-                    aria-hidden='true'
-                    className='r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-lrvibr r-m6rgpd r-1nao33i r-rxcuwo r-1777fci r-m327ed r-494qqr'
-                >
-                    <g>
-                        <path
-                            d='M21.742 21.75l-7.563-11.179 7.056-8.321h-2.456l-5.691 6.714-4.54-6.714H2.359l7.29 10.776L2.25 21.75h2.456l6.035-7.118 4.818 7.118h6.191-.008zM7.739 3.818L18.81 20.182h-2.447L5.29 3.818h2.447z'
-                            fill='#fff'
-                        ></path>
-                    </g>
-                </svg>
+                <Logo />
             </h1>
             <div className={style.right}>
                 <div className={style.title}>
