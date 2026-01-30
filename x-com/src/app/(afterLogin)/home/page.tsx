48b14a1642c238b355c22f11c3a1b8239ed7967b
gimeast <slasnrndu@gmail.com>
2026-01-31 00:01:43 +0900

    x-com 프로젝트 홈 페이지 상단 UI 작업
    - `home` 페이지 레이아웃 및 스타일 추가 (`page.tsx`, `page.module.css`)
    - 상단 탭 UI 및 게시물 작성 폼 구현
    - 더미 프로필 이미지(`dummy_profile.webp`) 추가
    - 불필요한 SVG 리소스 제거 (`file.svg`, `globe.svg`, `next.svg`, `vercel.svg`, `window.svg`)

--- x-com/src/app/(afterLogin)/home/page.tsx
+++ x-com/src/app/(afterLogin)/home/page.tsx
@@ -1,5 +1,38 @@
+'use client';
+
+import { Image as ImgButton } from 'lucide-react';
+import style from './page.module.css';
+import Image from 'next/image';
+import { useRef } from 'react';
+
 const Page = () => {
-    return <div>home</div>;
+    const fileRef = useRef<HTMLInputElement>(null);
+
+    const handleFile = () => {
+        fileRef?.current?.click();
+    };
+    return (
+        <div className={style.container}>
+            <nav className={style.tabs}>
+                <button>For you</button>
+                <button>Following</button>
+            </nav>
+            <form className={style.formWrapper}>
+                <Image className={style.profile} src='/dummy_profile.webp' alt='프로필' width={50} height={50} />
+                <div>
+                    <textarea cols={50} rows={2} placeholder={"What's happening?"} />
+                    <div className={style.buttonWrapper}>
+                        <input type='file' ref={fileRef} multiple hidden />
+                        <button type='button' className={style.imgButton} onClick={handleFile}>
+                            <ImgButton />
+                        </button>
+                        <button type='submit'>Post</button>
+                    </div>
+                </div>
+            </form>
+            <article>testes</article>
+        </div>
+    );
 };
 
 export default Page;
